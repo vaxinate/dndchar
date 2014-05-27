85fda560c8aa9fd46f02bd8bbd59b0694690f083
Adam Clarke <adamclarkewastaken@gmail.com>
2014-05-27 17:43:48 -0500

    fuck jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,17 +93,6 @@ module.exports = function (grunt) {
             },
             server: '.tmp'
         },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                'app/scripts/{,*/}*.js',
-                '!app/scripts/vendor/*',
-                'test/spec/{,*/}*.js'
-            ]
-        },
         mocha: {
             all: {
                 options: {
@@ -276,7 +265,6 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'jshint',
         'test',
         'build'
     ]);
