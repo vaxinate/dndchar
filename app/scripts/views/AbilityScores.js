48124da99a43082573c53fe26584f289a6e3ab36
Adam Clarke <adamclarkewastaken@gmail.com>
2014-05-27 17:48:47 -0500

    lol 4 space tabs

--- app/scripts/views/AbilityScores.js
+++ app/scripts/views/AbilityScores.js
@@ -1,12 +1,12 @@
 define([
-    'backbone',
-    'marionette',
-    'text!templates/AbilityScores._'
+  'backbone',
+  'marionette',
+  'text!templates/AbilityScores._'
 ],
 
 function (Backbone, Marionette, template) {
-    return Backbone.Marionette.ItemView.extend({
-        template: _.template(template),
-        events: {}
-    });
+  return Backbone.Marionette.ItemView.extend({
+    template: _.template(template),
+    events: {}
+  });
 });
