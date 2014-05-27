c7dffb503e7d4a698da09378bfcd101b23e5a4d4
Adam Clarke <adamclarkewastaken@gmail.com>
2014-05-27 17:44:52 -0500

    templates or something

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,7 @@
 requirejs.config({
     deps: ['main'],
     paths: {
+        text: '../bower_components/text/text',
         backbone: '../bower_components/backbone/backbone',
         jquery: '../bower_components/jquery/jquery',
         marionette: '../bower_components/marionette/lib/core/amd/backbone.marionette',
@@ -8,7 +9,6 @@ requirejs.config({
         'backbone.wreqr' : '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
         'backbone.eventbinder' : '../bower_components/backbone.eventbinder/lib/amd/backbone.eventbinder',
         'backbone.babysitter' : '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
-        templates: 'templates/compiled',
     },
     shim: {
         backbone: {
